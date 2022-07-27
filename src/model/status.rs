3bfea5a2340696a545b2acd57ec14bf639e968ca
workk <pleasedontspammail@gmail.com>
2022-07-26 17:00:40 -0700

    enforce tabs, which are better

--- src/model/status.rs
+++ src/model/status.rs
@@ -2,22 +2,22 @@ use serde::Deserialize;
 
 #[derive(Debug)]
 pub enum Status {
-    Offline,
-    Online,
-    InGroup,
+	Offline,
+	Online,
+	InGroup,
 }
 
 impl<'de> Deserialize<'de> for Status {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let s = u32::deserialize(deserializer)?;
-        match s {
-            0 => Ok(Status::Offline),
-            1 => Ok(Status::Online),
-            2 => Ok(Status::InGroup),
-            _ => Err(serde::de::Error::custom("invalid status")),
-        }
-    }
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		let s = u32::deserialize(deserializer)?;
+		match s {
+			0 => Ok(Status::Offline),
+			1 => Ok(Status::Online),
+			2 => Ok(Status::InGroup),
+			_ => Err(serde::de::Error::custom("invalid status")),
+		}
+	}
 }
